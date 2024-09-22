658b9f04f8e8c68aa6f353c0fdf99e81b4ee541b
Alex P <a117a117a@gmail.com>
2024-09-22 17:54:49 +0200

    completed exercise 3 in week 3 of the JS module

--- 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week3/assignment/ex3-lemonAllergy.test.js
@@ -25,26 +25,27 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasket, unwantedFruit) {
+  const sanitizedBasket = fruitBasket.slice();
+  return sanitizedBasket.filter(fruit => fruit !== unwantedFruit);
 }
 
 // ! Unit tests (using Jest)
 describe('js-wk3-ex3-lemonAllergy', () => {
   test('sanitizeFruitBasket should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('sanitizeFruitBasket should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    sanitizeFruitBasket(fruitBasket, 'lemon');
+    expect(fruitBasket).toEqual(originalFruitBasketContents);
   });
 
   test('sanitizeFruitBasket should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const result = sanitizeFruitBasket(fruitBasket, 'lemon');
+    const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+    expect(result).toEqual(expected);
   });
 });
