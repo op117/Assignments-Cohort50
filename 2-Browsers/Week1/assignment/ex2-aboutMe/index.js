01384adfea7307f87a887e41edc3588474869c36
Alex P <a117a117a@gmail.com>
2024-09-23 15:44:04 +0200

    completed exercise 2 in week1 of the Browsers module

--- 2-Browsers/Week1/assignment/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/assignment/ex2-aboutMe/index.js
@@ -8,4 +8,11 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
 
-// TODO add your JavaScript code here.
+document.getElementById('nickname').textContent = 'Alex P';
+document.getElementById('fav-food').textContent = 'eggs';
+document.getElementById('hometown').textContent = 'Steenwijk';
+
+const listItems = document.querySelectorAll('li');
+listItems.forEach(item => {
+  item.className = 'list-item';
+});
