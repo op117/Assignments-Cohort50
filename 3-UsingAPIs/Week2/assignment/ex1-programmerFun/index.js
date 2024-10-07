bae4820388a996f5e64fb09495d2ed78ae2eedba
Alex P <a117a117a@gmail.com>
2024-10-07 13:01:46 +0200

    completed ex1-programmerFun

--- 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
@@ -17,28 +17,41 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  return fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw new Error(`Network error: ${error.message}`);
+  });
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
+  const img = document.createElement('img');
+  img.src = data.img;
+  img.alt = data.alt;
+  img.title = data.title;
+  document.body.appendChild(img);
   console.log(data);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
+  const errorElement = document.createElement('h1');
+  errorElement.textContent = `Error: ${error.message}`;
+  document.body.appendChild(errorElement);
   console.log(error);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);
