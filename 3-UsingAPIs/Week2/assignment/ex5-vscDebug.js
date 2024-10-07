744b27da02e106e6e667d051de9fa7eca3ca069c
Alex P <a117a117a@gmail.com>
2024-10-07 19:02:50 +0200

    completed ex5-vscDebug

--- 3-UsingAPIs/Week2/assignment/ex5-vscDebug.js
+++ 3-UsingAPIs/Week2/assignment/ex5-vscDebug.js
@@ -10,8 +10,23 @@ async function getData(url) {
 
 function renderLaureate({ knownName, birth, death }) {
   console.log(`\nName: ${knownName.en}`);
-  console.log(`Birth: ${birth.date}, ${birth.place.locationString}`);
-  console.log(`Death: ${death.date}, ${death.place.locationString}`);
+
+  const birthPlace = birth.place && birth.place.locationString && birth.place.locationString.en
+    ? birth.place.locationString.en
+    : "Unknown location";
+
+  console.log(`Birth: ${birth.date}, ${birthPlace}`);
+
+  if (death) {
+    const deathPlace = death.place && death.place.locationString && death.place.locationString.en
+      ? death.place.locationString.en
+      : "Unknown location";
+
+    console.log(`Death: ${death.date}, ${deathPlace}`);
+    
+  } else {
+    console.log("Still alive");
+  }
 }
 
 function renderLaureates(laureates) {
@@ -20,10 +35,15 @@ function renderLaureates(laureates) {
 
 async function fetchAndRender() {
   try {
-    const laureates = getData(
+    const data = await getData(
       'http://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
     );
-    renderLaureates(laureates);
+
+    if (data && Array.isArray(data.laureates)) {
+      renderLaureates(data.laureates);
+    } else {
+      console.error('Laureates data is not available or is not an array.');
+    }
   } catch (err) {
     console.error(`Something went wrong: ${err.message}`);
   }
